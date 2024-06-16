2f673559f675a911687a3c6716164a219ce2e2c0
Tahdi Islam <tshdi60@gmail.com>
2024-06-16 17:46:02 +0600

    crate some apps

--- smart_care_server/settings.py
+++ smart_care_server/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'doctor',
+    'service',
+    'patient',
+    'contact_us',
+    'appointment',
 ]
 
 MIDDLEWARE = [
