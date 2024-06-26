f965e516d561f612a8dd4cbf59fe5f94d13a3144
Tahdi Islam <tshdi60@gmail.com>
2024-06-26 23:41:51 +0600

    create patient serializer

--- patient/views.py
+++ patient/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .serializers import PatientSerializer
+from rest_framework import viewsets
+from .models import Patient
 
 # Create your views here.
+class PatientViewSet(viewsets.ModelViewSet):
+    queryset = Patient.objects.all()
+    serializer_class = PatientSerializer
