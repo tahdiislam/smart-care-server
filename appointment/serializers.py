83fc746b4bf5a2f3ba67b33a40952a65dcd3820d
Tahdi Islam <tshdi60@gmail.com>
2024-07-01 21:51:12 +0600

    m

--- appointment/serializers.py
+++ appointment/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from .models import Appointment
 
 class AppointmentSerializer(serializers.ModelSerializer):
+    
     class Meta:
         model = Appointment
         fields = '__all__'
