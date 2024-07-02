2917965a46d467ed4a237941ab8a60cc7c4d74a6
Tahdi Islam <tshdi60@gmail.com>
2024-07-02 21:35:06 +0600

    add trusted origin

--- smart_care_server/settings.py
+++ smart_care_server/settings.py
@@ -29,6 +29,7 @@ SECRET_KEY = env('SECRET_KEY')
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
+CSRF_TRUSTED_ORIGINS = ['https://smart-care-587h.onrender.com', 'http://127.0.0.1:8000/']
 
 
 # Application definition
